abd1045510b58bf7afaca26d9e25334fe5aee39c
khyeonmin <lamb122@pusan.ac.kr>
2025-05-14 20:20:43 +0900

    Feat(package): install axios and fix deployment-related errors for testing

--- spec-trackr-app/src/com_sp.jsx
+++ spec-trackr-app/src/com_sp.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
 import "./com_sp.css";
 
 const jobCategories = ["생산/제조", "연구개발/설계", "IT/인터넷"];
@@ -11,24 +11,24 @@ const ComSp = ({ onSpecTabClick }) => {
   const [activeTab, setActiveTab] = useState("채용 공고");
 
   // ------------------------------------ fastAPI와 연결
-  const [companyOptions, setCompanyOptions] = useState([]);  // <- 동적으로 채울 회사 목록
+  // const [companyOptions, setCompanyOptions] = useState([]);  // <- 동적으로 채울 회사 목록
 
-  // 직업 선택 시 회사 목록 불러오기
-  useEffect(() => {
-    if (selectedJobCategory) {
-      axios
-      // fast API와 연결 -> 서버의 포트번호가 8000
-        .get(`http://localhost:8000/companies`, {
-          params: { job_category: selectedJobCategory },
-        })
-        .then((res) => {
-          setCompanyOptions(res.data.map((item) => item.name));
-        })
-        .catch((err) => {
-          console.error("회사 목록 불러오기 실패:", err);
-        });
-    }
-  }, [selectedJobCategory]);
+  // // 직업 선택 시 회사 목록 불러오기
+  // useEffect(() => {
+  //   if (selectedJobCategory) {
+  //     axios
+  //     // fast API와 연결 -> 서버의 포트번호가 8000
+  //       .get(`http://localhost:8000/companies`, {
+  //         params: { job_category: selectedJobCategory },
+  //       })
+  //       .then((res) => {
+  //         setCompanyOptions(res.data.map((item) => item.name));
+  //       })
+  //       .catch((err) => {
+  //         console.error("회사 목록 불러오기 실패:", err);
+  //       });
+  //   }
+  // }, [selectedJobCategory]);
 
   // ------------------------------------ fastAPI와 연결
   
@@ -83,11 +83,11 @@ const ComSp = ({ onSpecTabClick }) => {
             >
               <option>회사를 선택하세요</option>
               {/* fastAPI와 연결 --------------------------------*/}
-              {companyOptions.map((company, index) => (
+              {/* {companyOptions.map((company, index) => (
                 <option key={index} value={company}>
                   {company}
                 </option>
-              ))}
+              ))} */}
               {/* fastAPI와 연결 --------------------------------*/}
             </select>
           </div>
