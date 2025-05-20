eee5623563c58aebb62dc13619039780561af089
fin <haein1012@pusan.ac.kr>
2025-05-20 17:45:02 +0900

    Feat(API): add requirements

---
+++ API/database.py
@@ -0,0 +1,14 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Railway의 DATABASE_URL 환경변수 사용
+DATABASE_URL = os.environ["DATABASE_URL"]
+
+# SQLAlchemy 엔진 및 세션 설정
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# 모델 베이스 클래스
+Base = declarative_base()
