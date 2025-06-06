08760d3aa1dddcc661ae54889e86851485924e41
Hoyoung Seo <xvwjstkvx2@naver.com>
2025-06-07 02:09:38 +0900

    Feat(wordcloud): add tableau wordcloud preprocessing  and embed component example for frontend developer
    태블로 워드클라우드 시각화를 위한 전처리 코드(.ipynb)와
    공개 워드클라우드를 React 컴포넌트에서 바로 임베드할 수 있는 JSX 파일을 추가. 화면에 띄울 때 참고
    
    - keywords 전처리 및 불용어 제거 포함한 Jupyter Notebook 작성
    - Tableau Public 공유 링크를 iframe으로 출력하는 TableauEmbed 컴포넌트 추가

---
+++ tableau_visualization_example/App.js
@@ -0,0 +1,15 @@
+// App.js
+
+import React from 'react';
+import TableauEmbed from './TableauEmbed';
+
+function App() {
+  return (
+    <div>
+      <h1>📊 워드클라우드 예시 프론트에 띄우기</h1>
+      <TableauEmbed />
+    </div>
+  );
+}
+
+export default App;
