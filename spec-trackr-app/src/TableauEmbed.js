54d53d7fdc0d758330efd35082eb98f7ca41b8e5
Yerdeee <154613415+Yerdeee@users.noreply.github.com>
2025-06-07 11:05:39 +0900

    Feat(frontend): add tableau visualization box and update tab name (#87)

---
+++ spec-trackr-app/src/TableauEmbed.js
@@ -0,0 +1,18 @@
+// TableauEmbed.js
+
+import React from 'react';
+
+const TableauEmbed = () => {
+  return (
+  <div style={{ width: '100%', maxWidth: '1400px', margin: '0 auto' }}>
+    <iframe
+      src="https://public.tableau.com/views/_17492253239990/1?:embed=yes&:showVizHome=no"
+      style={{ width: '100%', height: '800px', border: 'none' }}
+      allowFullScreen
+      title="Tableau 워드클라우드"
+    ></iframe>
+</div>  
+  );
+};
+
+export default TableauEmbed;
